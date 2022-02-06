82c4debdb59632f07103174e6057b0bf177fcd5f
ruthjelimo <ruthjmimo@gmail.com>
2022-02-07 00:06:27 +0300

    added functionality

--- quote/Quote/src/app/quote.ts
+++ quote/Quote/src/app/quote.ts
@@ -1,7 +1,5 @@
-export class Quote {
-    // id!: number;
-    // authorName!:string;
-    // quote!:string;
+export class  Quote{
+  
     showQuote: boolean;
     constructor(public id: number,public authorName: string,public description: string,public genre:string,public publishDate: Date){
       this.showQuote=false;
