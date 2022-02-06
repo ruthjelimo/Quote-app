e5dc76b49af5f5d1011b010343bb15f81ca19c1f
ruthjelimo <ruthjmimo@gmail.com>
2022-02-06 22:08:12 +0300

    added form

--- quote/Quote/src/app/app.module.ts
+++ quote/Quote/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { DateCountPipe } from './date-count.pipe';
     QuoteComponent,
     QuoteDetailsComponent,
     DateCountPipe,
+    QuoteFormComponent,
 
   ],
   imports: [
